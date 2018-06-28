247613842a58bd37066f0b9539ddf3283cf1a771
Henri Gauffriau <henri.gauffriau@mazedia.fr>
2018-06-28 16:38:45 +0200

    update header

--- src/index.php
+++ src/index.php
@@ -1,21 +1,21 @@
 <?php include("header.php"); ?>
 
 	<main role="main" aria-label="Content">
-		<div class="wrapper">		
+		<div class="wrapper">
 			<pre class="template_name">index.php</pre>
 			<!-- section -->
 			<section>
-				
-				<h1>title</h1>
-			
+
+				<h1>titless</h1>
+
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 			</section>			<!-- /section -->
 		</div>
 	</main>
-	
+
 	<aside class="wrapper">
 
 	</aside>
-	
+
 <?php include("footer.php"); ?>
